149954ee551c0f4aa7a661309c6eb304f22281db
kaushal kishore <kaushalkishore@kaushals-MacBook-Pro.local>
2019-12-07 22:24:08 +0530

    Add initia commit

---
+++ src/main/java/com/devils/backend/devilsapi/DevilsApiApplication.java
@@ -0,0 +1,13 @@
+package com.devils.backend.devilsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevilsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevilsApiApplication.class, args);
+	}
+
+}
